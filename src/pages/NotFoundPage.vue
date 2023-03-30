fa84b17d948ad29346966d0d2c98ef46fea47642
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-30 18:17:32 +0200

    Create page not found

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+
+}
+</script>
+
+<template>
+    <section id="not-found-page">
+        <h1>404| Not Found</h1>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+#not-found-page {
+    height: calc(100vh - 80px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        font-size: 120px;
+        color: #4e598c;
+    }
+}
+</style>
