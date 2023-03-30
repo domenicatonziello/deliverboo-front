07858dc5b496d36848b0740454a75e3c6bfd9a17
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-30 18:16:24 +0200

    import fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
 import { router } from './routes/';
 
+// IMPORT FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+// import { faYoutube, faTwitch, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faStar, farStar, faMagnifyingGlass, faCartShopping);
+
 const app = createApp(App);
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.mount('#app');
