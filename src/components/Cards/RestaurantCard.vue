f8c15f04b9e74eb5de73f97218dbe99155e1892a
Antonio-Italiano <antoitalia221@gmail.com>
2023-04-11 15:15:50 +0200

    fixed src image

--- src/components/Cards/RestaurantCard.vue
+++ src/components/Cards/RestaurantCard.vue
@@ -20,7 +20,7 @@ export default {
 <template>
     <router-link :to="'restaurants/' + restaurant.id" @click='setActive()' class="card" :class="{ active: active }"
         style="width: 10rem;">
-        <img :src="restaurant.image" class="card-img-top" alt="...">
+        <img :src="restaurant.logo" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ restaurant.name }}</h5>
         </div>
