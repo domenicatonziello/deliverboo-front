1ff883c41b7044b57c8bcd52f50acf184bf56b17
Antonio-Italiano <antoitalia221@gmail.com>
2023-04-13 11:27:55 +0200

    imported CartContent Store modifched layoutStructure

--- src/pages/RestaurantPage.vue
+++ src/pages/RestaurantPage.vue
@@ -1,14 +1,17 @@
 <script>
+import { store } from '../data/store';
 import axios from 'axios';
 import FoodCard from '../components/Cards/FoodCard.vue';
+import CartContent from '../components/content/CartContent.vue';
 const baseUri = 'http://127.0.0.1:8000/api/';
 export default {
     name: 'RestaurantPage',
     data: () => ({
+        store,
         menu: [],
         name: '',
     }),
-    components: { FoodCard },
+    components: { FoodCard, CartContent },
     methods: {
         fetchRestaurants() {
             axios.get(baseUri + 'restaurants/' + this.$route.params.id)
@@ -33,14 +36,36 @@ export default {
 
 <template>
     <div class="container">
+        <div class="row">
+            <div :class="!store.cart ? 'col-12' : 'col-10'">
+                <h1 class="my-5 text-center">Menù ristorante <span>{{ this.name }}</span></h1>
+                <div class="d-flex flex-wrap pt-5 justify-content-center gap-2">
+                    <food-card v-for="food in menu" :key="food.id" :food="food"></food-card>
+                </div>
+            </div>
+            <div class="col-2" v-if="store.cart">
+                <div class="cart">
+                    <cart-content :foodCart="store.foodsCart"></cart-content>
+                </div>
+            </div>
 
-        <h1 class="my-5 text-center">Menù ristorante <span>{{ this.name }}</span></h1>
-        <div class="d-flex flex-wrap pt-5 justify-content-center gap-2">
-            <food-card v-for="food in menu" :key="food.id" :food="food"></food-card>
         </div>
+
+
+
+
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+.cart {
+    background-color: #fff;
+    // struttura
+    min-height: 50px;
+    width: 220px;
+
+    // stile
+    border-radius: 20px;
+    border: 2px solid black;
+}
 </style>
