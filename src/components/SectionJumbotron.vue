327eb8af45a1ef6770e0b9b3ab15175285f85c30
domenicatonziello <domenica.tonziello10@gmail.com>
2023-04-13 17:39:58 +0200

    merge

--- src/components/SectionJumbotron.vue
+++ src/components/SectionJumbotron.vue
@@ -5,15 +5,20 @@ export default {
 </script>
 
 <template lang="">
-    <section id="jumbotron">
-        <slot></slot>
-    </section>
+  <section id="jumbotron">
+    <slot></slot>
+  </section>
 </template>
 
 <style lang="scss">
 #jumbotron {
     min-height: 100vh;
     // padding-bottom: 20px;
-    background: linear-gradient(297.36deg, #FF8C42 55%, #FEB889 55%, #FEB07C 87.32%, #FCFBF7 143.55%);
+    background: linear-gradient(297.36deg,
+            #ff8c42 55%,
+            #feb889 55%,
+            #feb07c 87.32%,
+            #fcfbf7 143.55%);
 }
 </style>
+
